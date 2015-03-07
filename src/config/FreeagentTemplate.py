bbd26a893bb476846e822623d1642c25e1b48a32
John Evans <john@grandadevans.com>
2015-03-07 01:57:48 +0000

    TDD config for Freeagent OAuth stuff

---
+++ src/config/FreeagentTemplate.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+"""
+Docblock
+"""
+
+# import built in modules
+
+# import Third party
+
+# import local
+
+__author__ = "John Evans <john@grandadevans.com?"
+__copyright__ = "Copyright 2015, John Evans"
+__credits__ = ["John Evans <john@grandadevans.com>"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "John Evans"
+__email__ = "john@grandadevans.com"
+__status__ = "Development"
+
+class Freeagent:
+
+    def ob(self):
+        return {
+            "freeagent_API_auth_ID": "",
+            "freeagent_API_auth_secret": ""
+        }
---
+++ tests/config/test_config.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+"""
+Tests to make sure that we have the correct config keys in a template file
+
+The system will be responsible for converting this template into the correct
+config file once it is installed. But this way it keeps the real ie my
+API OAuth data out of the Version Control System
+"""
+
+import pytest
+from ...src.config.FreeagentTemplate import Freeagent
+
+class TestConfig:
+
+    def test_we_have_a_blank_oauth_id(self):
+        ob = Freeagent().ob()
+        print(ob)
+        assert(ob['freeagent_API_auth_ID'] == "")
+
+    def test_we_have_a_blank_oauth_secret(self):
+        ob = Freeagent().ob()
+        assert(ob['freeagent_API_auth_secret'] == "")
