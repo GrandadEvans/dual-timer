4c025ba098744e3d275414ddf1b7d55769412978
John Evans <john@grandadevans.com>
2015-03-16 17:01:18 +0000

    Small script that allows the monitoring of the current (active) z server window title

---
+++ DualTimer/Src/title.py
@@ -0,0 +1,26 @@
+from gi.repository import Gtk
+from gi.repository import GLib
+from gi.repository import Wnck
+
+class WindowTitle(object):
+    def __init__(self):
+        self.title = None
+        GLib.timeout_add(100, self.get_title)
+
+    def get_title(self):
+        try:
+            Gtk.main_iteration()
+            screen = Wnck.Screen.get_default()
+            screen.force_update()
+            active = screen.get_active_window()
+            title = active.get_name()
+            if self.title != title:
+                self.title = title
+                print(title)
+        except AttributeError:
+            pass
+        return True
+
+
+WindowTitle()
+Gtk.main()
